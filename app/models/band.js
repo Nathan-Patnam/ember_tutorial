ec1efcc36fa95f0e0d93d0cb297aaea9d9b46e65
Nathan Patnam <npatnam@salesforce.com>
2019-05-24 15:37:51 -0700

    create band model

---
+++ app/models/band.js
@@ -0,0 +1,8 @@
+// app/models/band.js
+import EmberObject, { computed } from '@ember/object'; import { dasherize } from '@ember/string';
+export default EmberObject.extend({
+    name: '',
+    slug: computed('name', function () {
+        return dasherize(this.name);
+    })
+});
