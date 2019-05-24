43c2d5b3f3c7f9ee93ea6b7ca43fd1c0b968c1bd
Nathan Patnam <npatnam@salesforce.com>
2019-05-24 15:39:45 -0700

    Create band model to initalize no songs, if user passes in nothing

--- app/models/band.js
+++ app/models/band.js
@@ -1,7 +1,14 @@
 // app/models/band.js
-import EmberObject, { computed } from '@ember/object'; import { dasherize } from '@ember/string';
+import EmberObject, { computed } from '@ember/object';
+import { dasherize } from '@ember/string';
+
 export default EmberObject.extend({
     name: '',
+    init() {
+        this._super(...arguments); if (!this.songs) {
+            this.set('songs', []);
+        }
+    },
     slug: computed('name', function () {
         return dasherize(this.name);
     })
